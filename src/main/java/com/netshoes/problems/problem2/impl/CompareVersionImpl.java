84c61612d04471cdee5b93ed06f9dc229a779f2e
Vitor Gomes Miranda <vigmiranda@gmail.com>
2018-04-07 12:37:15 -0300

    Questão 2

--- src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
+++ src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
@@ -1,11 +1,35 @@
 package com.netshoes.problems.problem2.impl;
 
+import java.util.Arrays;
+
 // Nao alterar o nome dessa classe!
 public class CompareVersionImpl {
 
     // Nao alterar a assinatura desse metodo
     public static int compareVersion(String version1, String version2) {
-        // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-        return 2;
+       Integer ret = 0;
+       boolean check = true;
+        String v1[] = version1.split("[.]");
+        String v2[] = version2.split("[.]");
+
+
+
+        for(int x =0; x < v1.length; x++)
+        {
+            if(Integer.parseInt(v1[x]) >  Integer.parseInt(v2[x]) && check)
+            {
+                ret = 1;
+                check = false;
+            }
+            else if(Integer.parseInt(v2[x]) >  Integer.parseInt(v1[x]) && check){
+                ret = -1;
+                check = false;
+            }
+        }
+
+        return ret;
     }
+
+
+
 }
